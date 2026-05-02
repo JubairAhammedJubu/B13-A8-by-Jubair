44450a89969a9e57794d00be7fcd13bd93008caa
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-05-03 01:30:41 +0600

    loading notfound page complete

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Montserrat, Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+
+export const metadata = {
+  title: "Bookify",
+  description: "Best books portal in Bangladesh",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" data-theme="light" className={`h-full antialiased`}>
+      <body className={`${poppins.className} min-h-full flex flex-col`}>
+        {children}
+      </body>
+    </html>
+  );
+}
