c1287fe71ca18bd5f031c3465004c7547803c7fd
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-05-04 18:39:53 +0600

    Add your commit

--- src/lib/data.js
+++ src/lib/data.js
@@ -4,9 +4,22 @@ import path from "path";
 export async function getBooks() {
   const filePath = path.join(process.cwd(), "public", "books.json");
 
+<<<<<<< HEAD
   const jsonData = fs.readFileSync(filePath, "utf-8");
 
   return JSON.parse(jsonData);
+=======
+  try {
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+
+    console.log("RAW DATA:", jsonData.slice(0, 100));
+
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error("ERROR IN getBooks:", error);
+    return [];
+  }
+>>>>>>> d4a4325 (chek)
 }
 
 export async function getBookById(id) {
