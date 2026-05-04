c162947628d5acd61e907d4972630d1c2ba1f52f
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-05-04 18:52:42 +0600

    commit message

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,34 +1,14 @@
-"use client";
-
 import Link from "next/link";
-import {useEffect, useState} from "react";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
-import "animate.css";
+import {getBooks} from "@/lib/data";
 
-export default function Home() {
-  const [booksData, setBooksData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  // Fetch from public folder
-  useEffect(() => {
-    fetch("/books.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setBooksData(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error("Failed to load books:", err);
-        setLoading(false);
-      });
-  }, []);
-
+export default async function Home() {
+  const booksData = await getBooks();
   const featuredBooks = booksData.slice(0, 4);
 
   return (
     <div className="container p-3 md:p-0 mx-auto my-10">
-      {/* HERO */}
       <section className="hero min-h-96 bg-linear-to-r from-indigo-500 to-purple-600 text-white rounded-t-xl">
         <div className="text-center">
           <h1 className="text-5xl md:text-6xl font-bold">
@@ -46,7 +26,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* MARQUEE */}
       <div className="flex justify-between gap-4 items-center bg-gray-200 py-4 px-3 rounded-b-xl">
         <button className="btn bg-red-500 text-white">Latest</button>
 
@@ -56,47 +35,41 @@ export default function Home() {
         </Marquee>
       </div>
 
-      {/* FEATURED BOOKS */}
       <section className="py-10 md:py-20">
         <h2 className="text-3xl md:text-5xl font-bold text-center mb-8 md:mb-16">
           Featured Books
         </h2>
 
-        {loading ? (
-          <p className="text-center">Loading books...</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-            {featuredBooks.map((book) => (
-              <div key={book.id} className="card bg-base-100 shadow-lg">
-                <figure>
-                  <Image
-                    src={book.image_url}
-                    alt={book.title}
-                    width={400}
-                    height={400}
-                    className="h-60 w-full object-cover"
-                  />
-                </figure>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+          {featuredBooks.map((book) => (
+            <div key={book.id} className="card bg-base-100 shadow-lg">
+              <figure>
+                <Image
+                  src={book.image_url}
+                  alt={book.title}
+                  width={400}
+                  height={400}
+                  className="h-60 w-full object-cover"
+                />
+              </figure>
 
-                <div className="card-body">
-                  <h2 className="card-title">{book.title}</h2>
-                  <p className="text-sm text-gray-500">{book.author}</p>
+              <div className="card-body">
+                <h2 className="card-title">{book.title}</h2>
+                <p className="text-sm text-gray-500">{book.author}</p>
 
-                  <div className="card-actions justify-end">
-                    <Link href={`/books/${book.id}`}>
-                      <button className="btn btn-sm bg-linear-to-r from-indigo-600 to-purple-600 text-white">
-                        View Details
-                      </button>
-                    </Link>
-                  </div>
+                <div className="card-actions justify-end">
+                  <Link href={`/books/${book.id}`}>
+                    <button className="btn btn-sm bg-linear-to-r from-indigo-600 to-purple-600 text-white">
+                      View Details
+                    </button>
+                  </Link>
                 </div>
               </div>
-            ))}
-          </div>
-        )}
+            </div>
+          ))}
+        </div>
       </section>
 
-      {/* WHY CHOOSE US */}
       <section className="bg-gray-50 md:p-20 py-10 rounded-2xl text-center">
         <h2 className="text-2xl md:text-[48px] font-bold text-gray-800 mb-3">
           Why Choose Bookify?
