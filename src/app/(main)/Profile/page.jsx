c50d8c335a7e99ae9fc40429a3f91bcc6fbd2ccf
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-05-04 16:21:51 +0600

    check

--- src/app/(main)/Profile/page.jsx
+++ src/app/(main)/Profile/page.jsx
@@ -1,45 +1,57 @@
-"use client";
+// "use client";
 
-import {authClient} from "@/lib/auth-client";
-import Image from "next/image";
-import {useRouter} from "next/navigation";
-import userImg from "@/assets/user.png"
+// import {authClient} from "@/lib/auth-client";
+// import Image from "next/image";
+// import {useRouter} from "next/navigation";
+// import userImg from "@/assets/user.png"
 
-const ProfilePage = () => {
-  const {data} = authClient.useSession();
-  const user = data?.user;
-  const router = useRouter();
+// const ProfilePage = () => {
+//   const {data} = authClient.useSession();
+//   const user = data?.user;
+//   const router = useRouter();
 
+//   return (
+//     <div className="min-h-[60vh] flex items-center justify-center bg-base-200 px-4">
+//       <div className="card w-full max-w-md bg-base-100 shadow-xl m-5 border border-base-300">
+//         {/* Avatar */}
+//         <div className="flex flex-col items-center pt-8 space-y-2">
+//           <div className="rounded-full">
+//             <Image
+//               src={user.image || userImg}
+//               alt={user.name || "User"}
+//               width={150}
+//               height={150}
+//               referrerPolicy="no-referrer"
+//             />
+//           </div>
+
+//           <h2 className="text-xl font-bold">{user.name}</h2>
+//         </div>
+
+//         {/* Actions */}
+//         <div className="card-body pt-0 items-center">
+//           <button
+//             onClick={() => router.push("/profile/update")}
+//             className="btn bg-linear-to-r from-indigo-500 to-purple-600 text-white "
+//           >
+//             Profile Update
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProfilePage;
+
+import React from 'react';
+
+const page = () => {
   return (
-    <div className="min-h-[60vh] flex items-center justify-center bg-base-200 px-4">
-      <div className="card w-full max-w-md bg-base-100 shadow-xl m-5 border border-base-300">
-        {/* Avatar */}
-        <div className="flex flex-col items-center pt-8 space-y-2">
-          <div className="rounded-full">
-            <Image
-              src={user.image || userImg}
-              alt={user.name || "User"}
-              width={150}
-              height={150}
-              referrerPolicy="no-referrer"
-            />
-          </div>
-
-          <h2 className="text-xl font-bold">{user.name}</h2>
-        </div>
-
-        {/* Actions */}
-        <div className="card-body pt-0 items-center">
-          <button
-            onClick={() => router.push("/profile/update")}
-            className="btn bg-linear-to-r from-indigo-500 to-purple-600 text-white "
-          >
-            Profile Update
-          </button>
-        </div>
-      </div>
+    <div>
+      
     </div>
   );
 };
 
-export default ProfilePage;
+export default page;
